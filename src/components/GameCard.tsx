4b2fe0a3af639aedd9c1f05308765c107f0d9239
LabPetro <lamppetro30@outlook.com>
2024-09-30 14:03:26 +0300

    Separate interfaces to entities

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from '../entities/Game';
 import PlatFormIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImage from "../services/image-url";
@@ -22,11 +22,9 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          <Link to={'/games/' + game.slug}>
-           {game.name}
-          </Link>
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
         </Heading>
-        <Emoji rating={game.rating_top}/>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
   );
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,22 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import ms from 'ms';
+import ms from "ms";
 import APIClient from "../services/api-client";
 import platforms from "../data/platforms";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: ms('24h'),
-    initialData: { count: platforms.length, results: platforms}
+    staleTime: ms("24h"),
+    initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;
